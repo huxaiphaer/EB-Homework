08819350ab10727d9569c98b3762f86813b65d4e
Cristian Suárez <cristianjs19@gmail.com>
2019-05-10 08:33:49 -0300

    Create Guess class. Improvements in functions
    Create Guess class and its functions. Improvements in userNum() function
    to get a 4 not repeated digit numbers.

--- tarea1.py
+++ tarea1.py
@@ -3,21 +3,20 @@ from random import randint
 class User():
 
 	def __init__(self):
-		self.name = input("""Indique su nombre para comenzar:
-			
-			""")
-	"""def name(self):
-		nombre = self.user
-		print(nombre)"""
+		self.name = input("Indique su nombre para comenzar:  ")
 
 	def userNum(self):
 		self.user_num = int(input("diga un número de 4 cifras:"))
+		for i in range(len(str(self.user_num))):
+			for j in range(i+1,len(str(self.user_num))):
+				while str(self.user_num)[i] == str(self.user_num)[j]:
+					self.user_num = int(input("Número inválido: ninguna cifra debe repetirse.\nDiga un número de 4 cifras:"))
+		if len(str(self.user_num)) != 4:
+			self.user_num = int(input("Número inválido.\nDiga un número de 4 cifras:"))
+class Host(User):
 
-class Computer(User):
-	#def __init__(self):
-		#self.user = input("Indique su nombre para comenzar")
 	def wellcome(self):
-		msg = "Bienvenido %s, en el siguiente juego usted debe adivinar cual es el número de 4 cifras que ha pensado el ordenador." % self.name
+		msg = "Bienvenido %s, en el siguiente juego usted debe adivinar cual es el número de 4 cifras que ha pensado el ordenador(ninguna se repite)." % self.name.upper()
 		print(msg)
 
 	def num(self):
@@ -29,18 +28,52 @@ class Computer(User):
 		num = ""
 		self.num = int(num.join(map(str,random)))
 		
-class Guess(Computer):
-	def guess(self):
-		pass
+class Guess(Host):
 
+	def guess(self):
+
+		ok = 0
+		reg = 0	
+		
+		self.user_num = list(map(int, str(self.user_num)))
+		self.num = list(map(int, str(self.num)))
+
+		if self.user_num[0] == self.num[0]: ok +=1
+		if self.user_num[1] == self.num[1]: ok +=1
+		if self.user_num[2] == self.num[2]: ok +=1
+		if self.user_num[3] == self.num[3]: ok +=1
+
+		if self.user_num[0] != self.num[0] and self.user_num[0] == self.num[1] or self.user_num[0] == self.num[2] or self.user_num[0] == self.num[3]: reg +=1
+		if self.user_num[1] != self.num[1] and self.user_num[1] == self.num[0] or self.user_num[1] == self.num[2] or self.user_num[1] == self.num[3]: reg +=1
+		if self.user_num[2] != self.num[2] and self.user_num[2] == self.num[0] or self.user_num[2] == self.num[1] or self.user_num[2] == self.num[3]: reg +=1
+		if self.user_num[3] != self.num[3] and self.user_num[3] == self.num[0] or self.user_num[3] == self.num[1] or self.user_num[3] == self.num[2]: reg +=1
+
+		print(self.user_num, self.num)
+		self.hint = "%d números OK | %d números REGULAR" % (ok, reg)
+		print(self.hint)
+		self.ok = ok
+		self.reg = reg
+
+	def win(self):
+		remain = 4 - self.ok
+		if self.ok < 4:
+			self.userNum()
+			empty_var = ""
+			self.num = int(empty_var.join(map(str,self.num)))
+			self.guess()
+			self.win()
+		else: print("¡¡FELICITACIONES ADIVINÓ EL NÚMERO!!")
 
 
 def main():
-    comenzar = Computer()
+    comenzar = Guess()
     comenzar.wellcome()
-    #comenzar.userNum()
     comenzar.num()
     print(comenzar.num)
+    comenzar.userNum()
+    comenzar.guess()
+    comenzar.win()
+
 
 if __name__ == "__main__":
 	main()
